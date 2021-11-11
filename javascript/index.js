c0bdb39d45f202363f227771808e9130a3c81dcb
Gaël <gael77@hotmail.fr>
2021-11-11 23:49:13 +0100

    Mise en page search-cards
    Doublons non supprimés

--- javascript/index.js
+++ javascript/index.js
@@ -1,35 +1,71 @@
 // DOM
 const chevron = document.querySelectorAll(".fa-chevron-down"); // Chevrons des boutons
+const searchCard = document.querySelectorAll(".search-card")
 const listContainer = document.querySelectorAll(".list-container"); // Containers
-const ingredientsContainer = document.getElementById("ingredients-container"); // Container des ingrédients
-const appliancesContainer = document.getElementById("appliances-container"); // Container des appareils
-const ustensilsContainer = document.getElementById("ustensils-container"); // Container des ustensiles
+const ingredientsUl = document.getElementById("ingredients-ul"); // Container des ingrédients
+const appliancesUl = document.getElementById("appliances-ul"); // Container des appareils
+const ustensilsUl = document.getElementById("ustensils-ul"); // Container des ustensiles
 const recipesContainer = document.getElementById("recipes-container"); // Container des recettes
 
-// Récupération des recettes
-fetch ("javascript/recipes.js")
+/* fetch ("javascript/recipes.js")
     .then(function(response) {
         if (response.ok) {
-            return response.json();
+            return response.json(); // Renvoie une promesse
         }
     })
     .then(function(value) {
         getRecipes(value)
+        console.log(value[0].ingredients)
     })
     .catch(function(err) { // Affichage d'une éventuelle erreur sur la console
         console.log(err)
-    })
+    }) */
 
-//Fonctions
-function getRecipes(recipesList) { //Affichage des recettes
-    function getIngredients(item) { // Affichage des ingrédients dans la card
-        return [
-            `<span class="ingredient">${item.ingredient}</span>:`,
-            item.quantity,
-            item.unit, `<br>`
-        ].join(" ");
+// Récupération des recettes  
+const recipesFile = async function() {
+    let response = await fetch ("javascript/recipes.js")
+    if (response.ok) {
+        let value = await response.json()
+        getRecipes(value)
+    } else {
+        console.error("Retour du serveur :", response.status);
+    }
+}
+recipesFile()
+    
+function getRecipes(recipesList) {
+    // Affichage des ingrédients dans la card
+    function getIngredients(item) {
+        return `<span class="ingredient">${item.ingredient}</span>: ${item.quantity || ""} ${item.unit || ""} <br>`;
     }
 
+    // Affichage des ingrédients
+    function getIngredientsList(item) {
+        return `<li>${item.ingredient}</li>,`
+    }
+    
+    const ingredientsArray = recipesList.map((recipe)=>{return `
+    ${recipe.ingredients.map(getIngredientsList).join(" ").split(",")}
+    `})
+    const ingredientsStr = ingredientsArray.filter((e,i)=>ingredientsArray.indexOf(e) == i).join(" ").replace(/[, ]+/g, " ")
+    ingredientsUl.innerHTML = ingredientsStr || ""
+    
+    // Affichage des appareils
+    const appliancesArray = recipesList.map((recipe)=>{return `<li>${recipe.appliance}</li>`})
+    const newAppliancesArray = appliancesArray.filter((e,i)=>appliancesArray.indexOf(e) == i)
+    appliancesUl.innerHTML = newAppliancesArray.join(" ") || ""
+    
+    // Affichage des ustensiles
+    function getUstensilsList(item) {
+        return `<li>${item}</li>,`
+    }
+    const ustensilsArray = recipesList.map((recipe)=>{return `
+    ${recipe.ustensils.map(getUstensilsList)}
+    `}).join(" ").split(",")
+    const ustensilsStr = ustensilsArray.filter((e,i)=>ustensilsArray.indexOf(e) == i).join(" ").replace(/[, ]+/g, " ")
+    ustensilsUl.innerHTML = ustensilsStr || ""
+
+    //Affichage des recettes
     const recipeHTMLString = recipesList.map((recipe)=>{return `
     <div class="recipe-card">
         <div class="recipe-img"></div>
@@ -39,60 +75,51 @@ function getRecipes(recipesList) { //Affichage des recettes
                 <span class="time"><i class="far fa-clock"></i> ${recipe.time} min</span>
             </div>
             <div class="recipe-instructions">
-                <aside>${recipe.ingredients.map(getIngredients)}</aside>
-                <article class="description">${recipe.description}</article>
+                <aside>${recipe.ingredients.map(getIngredients).join(" ")}</aside>
+                <article class="description">${recipe.description.substring(0,181)}${recipe.description.length > 181 ? "...":""/* Condition ternaire*/ }</article> 
             </div>
         </div>
     </div>
     `}).join(" ")
     recipesContainer.innerHTML = recipeHTMLString  
-    
-    // Affichage des ingrédients au clic sur le bouton
-    chevron[0].addEventListener("click", showIngredientsList) 
-    function showIngredientsList(item) {
-        dropdown(chevron[0]) // Retournement du chevron
-        ingredientsContainer.innerHTML = `${item.ingredient}`
-    }
-    
-    // Affichage des appareils au clic sur le bouton
-    chevron[1].addEventListener("click", showAppliancesList) 
-    function showAppliancesList(recipesList) {
-        dropdown(chevron[1]) // Retournement du chevron
-        const appliancesHTMLString = recipesList.map((recipe)=>{return `
-        <span class="appliance">${recipe.appliance}</span>
-        `}).join(" ")
-        appliancesContainer.innerHTML = appliancesHTMLString
-    }
-    
-    // Affichage des ustensiles au clic sur le bouton
-    chevron[2].addEventListener("click", showUstensilsList) 
-    function showUstensilsList(recipesList) {
-        dropdown(chevron[2]) // Retournement du chevron
-        const ustensilsHTMLString = recipesList.map((recipe)=>{return `
-        <span class="ustensils">${recipe.ustensils}</span>
-        `}).join(" ")
-        ustensilsContainer.innerHTML = ustensilsHTMLString
-    }
 }
 
-//Retournement du chevron des cards au clic
-function dropdown(chevron) { 
-    if (chevron.style.transform == "rotate(180deg)") {
-        chevron.style.transform = "";
+// Affichage des ingrédients au clic sur le bouton
+chevron[0].addEventListener("click", showIngredientsList) 
+function showIngredientsList() {
+    openCard(searchCard[0]) // Ouverture de la search-card
+    closeCard(searchCard[1])
+    closeCard(searchCard[2])
+}
+    
+// Affichage des appareils au clic sur le bouton
+chevron[1].addEventListener("click", showAppliancesList) 
+function showAppliancesList() {
+    openCard(searchCard[1]) // Ouverture de la search-card
+    closeCard(searchCard[0])
+    closeCard(searchCard[2])
+}
+
+// Affichage des ustensiles au clic sur le bouton
+chevron[2].addEventListener("click", showUstensilsList) 
+function showUstensilsList() {
+    openCard(searchCard[2]) // Ouverture de la search-card
+    closeCard(searchCard[0])
+    closeCard(searchCard[1])
+}
+
+// Ouverture de la searchCard + retournement chevron
+function openCard(searchCard) {
+    if (searchCard.classList.contains("active")) {
+        searchCard.classList.remove("active")
     } else {
-        chevron.style.transform = "rotate(180deg)";
+        searchCard.classList.add("active")
     }
 }
 
-/* // Retournement du chevron des cards au clic
-for (let i = 0; i < chevron.length; i++) {
-    const element = chevron[i];
-    element.addEventListener("click", dropdown)
-    function dropdown() {
-        if (element.style.transform == "rotate(180deg)") {
-            element.style.transform = "";
-        } else {
-            element.style.transform = "rotate(180deg)";
-        }
+// Fermeture des cards ouvertes
+function closeCard(searchCard) {
+    if (searchCard.classList.contains("active")) {
+        searchCard.classList.remove("active")
     }
-}*/
+}

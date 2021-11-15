7798ad0eb5a2baf1834d98013c11b02f0862f506
Gaël <gael77@hotmail.fr>
2021-11-15 14:14:13 +0100

    Méthode de filtrage searchbar

--- javascript/index.js
+++ javascript/index.js
@@ -1,4 +1,5 @@
 // DOM
+const searchBarInput = document.getElementById("search"); // Input de la barre de recherche
 const chevron = document.querySelectorAll(".fa-chevron-down"); // Chevrons des boutons
 const searchCard = document.querySelectorAll(".search-card")
 const listContainer = document.querySelectorAll(".list-container"); // Containers
@@ -7,26 +8,12 @@ const appliancesUl = document.getElementById("appliances-ul"); // Container des 
 const ustensilsUl = document.getElementById("ustensils-ul"); // Container des ustensiles
 const recipesContainer = document.getElementById("recipes-container"); // Container des recettes
 
-/* fetch ("javascript/recipes.js")
-    .then(function(response) {
-        if (response.ok) {
-            return response.json(); // Renvoie une promesse
-        }
-    })
-    .then(function(value) {
-        getRecipes(value)
-        console.log(value[0].ingredients)
-    })
-    .catch(function(err) { // Affichage d'une éventuelle erreur sur la console
-        console.log(err)
-    }) */
-
 // Récupération des recettes  
 const recipesFile = async function() {
     let response = await fetch ("javascript/recipes.js")
     if (response.ok) {
-        let value = await response.json()
-        getRecipes(value)
+        let recipes = await response.json()
+        getRecipes(recipes)
     } else {
         console.error("Retour du serveur :", response.status);
     }
@@ -39,34 +26,35 @@ function getRecipes(recipesList) {
         return `<span class="ingredient">${item.ingredient}</span>: ${item.quantity || ""} ${item.unit || ""} <br>`;
     }
 
-    // Affichage des ingrédients
+    // Affichage des ingrédients pour la search-card "Ingrédients"
     function getIngredientsList(item) {
         return `<li>${item.ingredient}</li>,`
     }
     
-    const ingredientsArray = recipesList.map((recipe)=>{return `
-    ${recipe.ingredients.map(getIngredientsList).join(" ").split(",")}
-    `})
-    const ingredientsStr = ingredientsArray.filter((e,i)=>ingredientsArray.indexOf(e) == i).join(" ").replace(/[, ]+/g, " ")
-    ingredientsUl.innerHTML = ingredientsStr || ""
+    const ingredientsArr = recipesList.map((recipe)=>{
+        return `${recipe.ingredients.map(getIngredientsList).join(" ").split(",")}`
+    })
+    const ingredientsHTMLString = ingredientsArr.filter((e,i)=>ingredientsArr.indexOf(e) == i).join(" ").replace(/[, ]+/g, " ")
+    ingredientsUl.innerHTML = ingredientsHTMLString || ""
     
-    // Affichage des appareils
-    const appliancesArray = recipesList.map((recipe)=>{return `<li>${recipe.appliance}</li>`})
-    const newAppliancesArray = appliancesArray.filter((e,i)=>appliancesArray.indexOf(e) == i)
-    appliancesUl.innerHTML = newAppliancesArray.join(" ") || ""
+    // Affichage des appareils dans la search-card "Appareils"
+    const appliancesArray = recipesList.map((recipe)=>{
+        return `<li>${recipe.appliance}</li>`
+    })
+    const appliancesHTMLString = appliancesArray.filter((e,i)=>appliancesArray.indexOf(e) == i).join(" ")
+    appliancesUl.innerHTML = appliancesHTMLString || ""
     
-    // Affichage des ustensiles
+    // Affichage des ustensiles dans la search-card "Ustensiles"
     function getUstensilsList(item) {
-        return `<li>${item}</li>,`
+        return `<li>${item}</li>`
     }
-    const ustensilsArray = recipesList.map((recipe)=>{return `
-    ${recipe.ustensils.map(getUstensilsList)}
-    `}).join(" ").split(",")
-    const ustensilsStr = ustensilsArray.filter((e,i)=>ustensilsArray.indexOf(e) == i).join(" ").replace(/[, ]+/g, " ")
-    ustensilsUl.innerHTML = ustensilsStr || ""
+    const ustensilsHTMLString = recipesList.map((recipe)=>{
+        return `${recipe.ustensils.map(getUstensilsList).join(" ")}`
+    }).join(" ")
+    ustensilsUl.innerHTML = ustensilsHTMLString || ""
 
-    //Affichage des recettes
-    const recipeHTMLString = recipesList.map((recipe)=>{return `
+    //Affichage des recettes sur la page
+    const recipeArray = recipesList.map((recipe)=>{return `
     <div class="recipe-card">
         <div class="recipe-img"></div>
         <div class="recipe-text">
@@ -76,12 +64,36 @@ function getRecipes(recipesList) {
             </div>
             <div class="recipe-instructions">
                 <aside>${recipe.ingredients.map(getIngredients).join(" ")}</aside>
-                <article class="description">${recipe.description.substring(0,181)}${recipe.description.length > 181 ? "...":""/* Condition ternaire*/ }</article> 
+                <article class="description">${recipe.description.substring(0,181)}${recipe.description.length > 181 ? "...":""}</article> 
             </div>
         </div>
     </div>
-    `}).join(" ")
-    recipesContainer.innerHTML = recipeHTMLString  
+    `})
+    let recipeHTMLString = recipeArray.join(" ")
+    recipesContainer.innerHTML = recipeHTMLString
+    console.log(recipesList);
+    
+    // Filtrage des recettes avec la search-bar
+    searchBarInput.addEventListener("input", recipeFilter)
+    function recipeFilter() {
+        if (searchBarInput.value.length >= 3) {
+            let searchArray = []
+            for (let i = 0; i < recipeArray.length; i++) {
+                const element = recipeArray[i];
+                let research = searchBarInput.value.toLowerCase()
+                if (element.toLowerCase().includes(research)) {
+                    searchArray.push(element)
+                }
+            }
+            if (searchArray.length === 0) {
+                recipesContainer.innerHTML = "Aucune recette ne correspond à votre critère... Vous pouvez chercher  « tarte aux pommes », « poisson », etc.";
+            } else {
+                recipesContainer.innerHTML = searchArray.join(" ")
+            }
+        } else {
+            recipesContainer.innerHTML = recipeHTMLString
+        }
+    }
 }
 
 // Affichage des ingrédients au clic sur le bouton
@@ -110,11 +122,7 @@ function showUstensilsList() {
 
 // Ouverture de la searchCard + retournement chevron
 function openCard(searchCard) {
-    if (searchCard.classList.contains("active")) {
-        searchCard.classList.remove("active")
-    } else {
-        searchCard.classList.add("active")
-    }
+    searchCard.classList.toggle("active")
 }
 
 // Fermeture des cards ouvertes
